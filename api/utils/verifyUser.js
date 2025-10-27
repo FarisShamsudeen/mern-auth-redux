30818d6c51d684a5c9fda9d96508e8c0a657c08d
Faris Shamsudeen <faris52342345@gmail.com>
2025-10-27 11:56:59 +0530

    Completed Update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,8 +5,13 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(errorHandler(401, "You need to Login"));
 
-  jwt.verify(token, process.env.JWT_SUCCESS, (err, user) => {
-    if (err) return next(errorHandler(403,"Token is not valid"));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log(token)
+      console.log(process.env.JWT_SECRET)
+      next(errorHandler(403, "Token is not valid"));
+      return
+    }
     req.user = user;
     next();
   })
